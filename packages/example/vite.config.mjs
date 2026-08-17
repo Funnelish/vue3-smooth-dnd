3d18a92c9bc7470835268aa647129207d6222572
Adnan Hidayat P <adnan@funnelish.com>
2026-08-17 14:13:51 +0700

    Revert vite-config

--- packages/example/vite.config.mjs
+++ packages/example/vite.config.mjs
@@ -1,15 +1,7 @@
-import { defineConfig, searchForWorkspaceRoot } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url';
-
-const smoothDndRoot = fileURLToPath(new URL('../../../smooth-dnd', import.meta.url));
 
 export default defineConfig({
   base: '/vue3-smooth-dnd/',
-  plugins: [vue()],
-  server: {
-    fs: {
-      allow: [searchForWorkspaceRoot(process.cwd()), smoothDndRoot]
-    }
-  }
+  plugins: [vue()]
 });
